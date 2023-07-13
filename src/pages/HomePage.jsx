950d30c85337b3887489720f1743583bc8034fa5
Marko Melle <markmelle1@gmail.com>
2023-07-13 21:07:29 +0200

    git init

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function HomePage() {
+   return (
+      <div>
+         <h1>Home Page</h1>
+         <p>
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+         voluptatum, quibusdam, quia, voluptate voluptas accusamus dolorum
+         officia, quos aperiam voluptatibus voluptatem? Quas, voluptatem
+         voluptatum. Voluptatibus, quisquam. Quisquam voluptatum, quibusdam,
+         quia, voluptate voluptas accusamus dolorum
+         </p>
+      </div>
+   );
+};
