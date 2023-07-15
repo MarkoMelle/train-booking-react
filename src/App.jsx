0946d138039718449ff293da8c28f6c1ac2575a4
Marko Melle <markmelle1@gmail.com>
2023-07-15 17:51:19 +0200

    add environment variables to .env.development and .env.production

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import HomePage from "./pages/HomePage";
 
 export default function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
     </Router>
   );
 }
-
-
