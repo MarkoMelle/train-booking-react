4ecf8fc3db5c65bcee3ac13a5f43754d1d8b1f07
Marko Melle <markmelle1@gmail.com>
2023-07-15 18:00:26 +0200

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
 export default function App() {
+  console.log(import.meta.env.VITE_PUBLIC_URL);
   return (
-    <Router basename={import.meta.env.PUBLIC_URL}>
+    <Router basename={import.meta.env.VITE_PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
